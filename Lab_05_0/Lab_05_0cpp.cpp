4e87435036eb77374a9a3518ca08c3e82959fdfa
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-11-03 12:36:45 +0200

    unity test commit

--- Lab_05_0/Lab_05_0cpp.cpp
+++ Lab_05_0/Lab_05_0cpp.cpp
@@ -13,13 +13,14 @@ int main()
 	cout << "s = "; cin >> s;
 	cout << "t = "; cin >> t;
 
-	double c = (1. * (h(s * s, 1 + t) + h(1, s * t)) / (1 + h(s, t) * h(s, t)));
+	double c = (1. * (h(pow(s, 2), 1 + t) + h(1, s * t)) / (1 + pow(h(s, t), 2)));
 
+	cout << h(2, -5) << endl;
 
 	cout << "c = " << c << endl;
 	return 0;
 }
 double h(const double a, const double b) // визначення
 {
-	return ((a + b + pow(a, 2) * pow(b, 2)) / (pow(a, 2) + pow(b, 2) * 1.));
+	return ((a + b + pow(a, 2) * pow(b, 2)) / ((pow(a, 2) + pow(b, 2)) * 1.));
 }
---
+++ UnitTest_05_0/UnitTest_05_0.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_05_0/Lab_05_0cpp.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest050
+{
+	TEST_CLASS(UnitTest050)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2, -5);
+			Assert::AreEqual(t, 3.34483, 0.0001);
+		}
+	};    
+}
